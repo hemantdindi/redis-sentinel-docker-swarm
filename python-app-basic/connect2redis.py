979fc311fe4e1ff4acc24549ab4b5b6624785214
hemantdindi <hemantdindi@users.noreply.github.com>
2020-06-13 13:24:33 +0530

    Updated py files

--- python-app-basic/connect2redis.py
+++ python-app-basic/connect2redis.py
@@ -1,11 +1,11 @@
 from redis.sentinel import Sentinel
 import redis
-#sentinel = Sentinel([('node01', 26379),('node02',26379),('node03',26379)],password='laSQL2019',socket_timeout=0.5)
-sentinel = Sentinel([('redis-sentinel', 26379)],password='laSQL2019',socket_timeout=0.5)
-host, port = sentinel.discover_master('master-node')
+#sentinel = Sentinel([('node01', 26379),('node02',26379),('node03',26379)],password='F9HaeCD6df24nLpn',socket_timeout=0.5)
+sentinel = Sentinel([('redis-sentinel', 26379)],password='7cBEcwf6mV36Rx3S',socket_timeout=0.5)
+host, port = sentinel.discover_master('redis-cache')
 print(host)
 print(port)
-redis_client = redis.StrictRedis(host=host,port=port,password='laSQL2019')
-redis_client.set('harbor', 'docker-registry')
-value=redis_client.get('harbor')
+redis_client = redis.StrictRedis(host=host,port=port,password='7cBEcwf6mV36Rx3S')
+redis_client.set('key', 'value')
+value=redis_client.get('key')
 print(value)
