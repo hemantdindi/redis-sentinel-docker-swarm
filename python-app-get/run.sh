9d5c7474a9b373d496c92fcb3de542ff6888f8db
hemantdindi <hemantdindi@users.noreply.github.com>
2020-06-13 14:10:49 +0530

    Update README.md

--- python-app-get/run.sh
+++ python-app-get/run.sh
@@ -1,3 +1,4 @@
 docker build -t hemantdindi/redis-py-get .
 docker  push hemantdindi/redis-py-get
+docker run --network redis-nw hemantdindi/redis-py-get sampleKey
 #docker run --network redis-nw hemantdindi/redis-py
--- python-app-set/run.sh
+++ python-app-set/run.sh
@@ -1,3 +1,3 @@
 docker build -t hemantdindi/redis-py-set .
 docker  push hemantdindi/redis-py-set
-#docker run --network redis-nw hemantdindi/redis-py
+docker run --network redis-nw hemantdindi/redis-py-set sampleKey sampleValue
